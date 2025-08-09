48d8c7da06aab758fb353a470948ce699e3717f8
zhaozike <1113840853@qq.com>
2025-08-09 22:00:34 +0800

    fix: 修正 Book.ts

---
+++ models/Book.ts
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from 'mongoose';
+
+const bookSchema = new Schema({
+  title: String,
+  author: String,
+  // 其他模型屬性...
+});
+
+const Book = mongoose.models.Book || mongoose.model('Book', bookSchema);
+
+export default Book;
